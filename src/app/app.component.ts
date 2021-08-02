26faac215c030892ccc50541c62ba3e9887d4e1c
carolinaguzman76 <carolinaguzman76@gmail.com>
2021-08-02 19:07:13 +0200

    Clase cuenta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Cuenta} from "./ejercicios/cuenta";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejercicios';
+
+  constructor() {
+    console.log('esto es el constructor de app.component');
+
+    const cuenta = new Cuenta('Ana');
+    const cuenta2 = new Cuenta('Luis', 2598);
+
+    const p = cuenta.titular;
+    cuenta.titular = 'titular de la cuenta';
+
+    console.log(cuenta.ingresar(4));
+    console.log(cuenta.ingresar(5));
+    console.log(cuenta.ingresar(-1));
+
+    console.log(cuenta.retirar(2));
+    console.log(cuenta.retirar(10));
+  }
 }
--- src/app/ejercicios/cuenta.ts
+++ src/app/ejercicios/cuenta.ts
@@ -1,5 +1,45 @@
 export class Cuenta {
+  private _titular: string;
+  private _cantidad: number;
 
-  constructor() {
+  constructor(titular: string, cantidad?: number) {
+    this._titular = titular;
+
+    if (typeof cantidad === "number") {
+      this._cantidad = cantidad;
+    } else {
+      this._cantidad = 0;
+    }
   }
+
+
+  get cantidad(): number {
+    return this._cantidad;
+  }
+
+  get titular(): string {
+    return this._titular;
+  }
+
+  set titular(titular: string) {
+    this._titular = titular;
+  }
+
+  ingresar(importe: number): number {
+
+    if (importe >= 0) {
+      this._cantidad = this._cantidad + importe;
+    }
+    return this._cantidad;
+  }
+
+  retirar(importe: number): number {
+    this._cantidad = this._cantidad - importe;
+
+    if (this._cantidad < 0) {
+      this._cantidad = 0;
+    }
+    return this._cantidad;
+  }
+
 }
